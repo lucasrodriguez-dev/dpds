75d699cd20d0f3252831ed5b6cd6ac2a89b353e9
Lucas Rodríguez <lucascsrodriguez@gmail.com>
2026-07-04 01:08:20 -0300

    agrego header para ControladorExperimento. Ahora solo falta implementar todos los headers

---
+++ include/ControladorExperimento.h
@@ -0,0 +1,21 @@
+#ifndef CONTROLADOREXPERIMENTO_H
+#define CONTROLADOREXPERIMENTO_H
+
+#include "ManejadorExperimento.h"
+
+class ControladorExperimento {
+    public:
+        void altaExperimento(std::string,std::string);
+        std::vector<DTExperimento> listarExperimentos() const;
+        void asociarEventos(std::string,std::vector<DTEvento>);
+        DTEvento simularExperimento(std::string) const;
+        std::vector<DTSimulacionExperimento> simularExperimento(std::string,int) const;
+        void altaVariableAleatoria(std::string,std::string,std::string,DTDistribucion);
+        std::vector<DTVariableAleatoria> listarVariablesAleatorias() const;
+        float simularVariableAleatoria(std::string) const;
+        float consultarPropiedad(std::string,PropiedadNumerica) const; 
+        float evaluarFuncionDensidad(std::string,float) const;       
+        float evaluarFuncionDistribucionAcumulada(std::string,float) const;       
+};
+
+#endif
