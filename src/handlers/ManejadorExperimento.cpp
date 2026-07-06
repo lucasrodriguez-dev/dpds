afe23e104f0b41df2e70ceba5622e3de2255fb11
Lucas Rodríguez de Pena <100799757+lucasrodriguez-dev@users.noreply.github.com>
2026-07-06 12:49:52 +0000

    implemento ManejadorExperimento

---
+++ src/handlers/ManejadorExperimento.cpp
@@ -0,0 +1,30 @@
+#include "ManejadorExperimento.h"
+
+ManejadorExperimento* ManejadorExperimento::instancia = nullptr;
+
+ManejadorExperimento* ManejadorExperimento::getInstancia() {
+    if(instancia == nullptr){
+        instancia = new ManejadorExperimento();
+    }
+    return instancia;
+}
+
+void ManejadorExperimento::altaExperimento(std::string nombre, std::string descripcion) {
+    experimentos[nombre] = new Experimento(nombre, descripcion);
+}
+
+std::vector<DTExperimento> ManejadorExperimento::listarExperimentos() const {
+    std::vector<DTExperimento> retorno;
+    for(auto experimento: experimentos){
+        retorno.push_back(experimento.second->getDT());
+    }
+    return retorno;
+}
+
+Experimento* ManejadorExperimento::getExperimento(std::string nombre) const {
+    auto exp = experimentos.find(nombre);
+    if(exp != experimentos.end()){
+        return exp->second;
+    }
+    return nullptr;
+}
