55e8ddb211b252673fda575a08d35e4dc9b3ebde
Lucas Rodríguez de Pena <100799757+lucasrodriguez-dev@users.noreply.github.com>
2026-07-06 13:17:54 +0000

    implemento ControladorExperimento

--- include/controllers/ControladorExperimento.h
+++ include/controllers/ControladorExperimento.h
@@ -8,17 +8,18 @@ class ControladorExperimento {
         static const std::string EXPERIMENTO_NULO;
         std::string experimentoSeleccionado;
     public:
+        ControladorExperimento();
         void altaExperimento(std::string,std::string);
         std::vector<DTExperimento> listarExperimentos() const;
         void asociarEventos(std::string,std::vector<DTEvento>);
         DTEvento simularExperimento(std::string) const;
         std::vector<DTSimulacionExperimento> simularExperimento(std::string,int) const;
         void altaVariableAleatoria(std::string,std::string,std::string,DTDistribucion);
-        std::vector<DTVariableAleatoria> listarVariablesAleatorias() const;
-        float simularVariableAleatoria(std::string) const;
-        float consultarPropiedad(std::string,PropiedadNumerica) const; 
-        float evaluarFuncionDensidad(std::string,float) const;       
-        float evaluarFuncionDistribucionAcumulada(std::string,float) const;       
+        std::vector<DTVariableAleatoria> listarVariablesAleatorias(std::string);
+        float simularVariableAleatoria(std::string);
+        float consultarPropiedad(std::string,PropiedadNumerica);
+        float evaluarVariableAleatoria(std::string,float);
+        float evaluarFuncionDistribucionAcumulada(std::string,float);
 };
 
 #endif
