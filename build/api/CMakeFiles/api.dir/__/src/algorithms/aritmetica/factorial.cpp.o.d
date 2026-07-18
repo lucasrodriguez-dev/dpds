d19bd0e01fee08488a1ce711793125d9dfbf33b4
Lucas Rodríguez <lucascsrodriguez@gmail.com>
2026-07-18 01:10:13 -0300

    Ignorar carpeta build

--- build/api/CMakeFiles/api.dir/__/src/algorithms/aritmetica/factorial.cpp.o.d
+++
@@ -1,4 +0,0 @@
-api/CMakeFiles/api.dir/__/src/algorithms/aritmetica/factorial.cpp.o: \
- /home/lucas/Projects/personal/dpds/src/algorithms/aritmetica/factorial.cpp \
- /usr/include/stdc-predef.h \
- /home/lucas/Projects/personal/dpds/include/algorithms/aritmetica/factorial.h
--- build/api/CMakeFiles/api.dir/__/src/algorithms/combinatoria/combinatoria_factorial.cpp.o.d
+++
@@ -1,5 +0,0 @@
-api/CMakeFiles/api.dir/__/src/algorithms/combinatoria/combinatoria_factorial.cpp.o: \
- /home/lucas/Projects/personal/dpds/src/algorithms/combinatoria/combinatoria_factorial.cpp \
- /usr/include/stdc-predef.h \
- /home/lucas/Projects/personal/dpds/include/algorithms/aritmetica/factorial.h \
- /home/lucas/Projects/personal/dpds/include/algorithms/combinatoria/combinacion_factoriales.h
